f273aa754a538fd804d4c3106ae595a378e7180e
sanrenhe-zjut <sanrenhe888@gmail.com>
2019-11-28 12:51:05 +0800

    20191128 Winter

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,7 +1,11 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
+import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  *
@@ -19,8 +24,12 @@ import javax.servlet.http.HttpServletRequest;
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public  String index(HttpServletRequest request) {
+    public  String index(HttpServletRequest request,
+                         Model model) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -34,6 +43,9 @@ public class IndexController {
                 }
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
 
 /**
  *
@@ -16,4 +17,7 @@ public interface UserMapper {
 
     @Select("SELECT * FROM user WHERE token = #{token}")
     User findByToken(@Param("token") String token);
+
+    @Select("SELECT * FROM `user` WHERE id = #{id}")
+    User findById(@Param("id") Integer creator);
 }
