61104ac46cc59be891ed4bf6d63051d5812089c3
sanrenhe-zjut <sanrenhe888@gmail.com>
2019-11-30 13:53:29 +0800

    20191130

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
@@ -29,7 +30,9 @@ public class IndexController {
     private QuestionService questionService;
     @GetMapping("/")
     public  String index(HttpServletRequest request,
-                         Model model) {
+                         Model model,
+                         @RequestParam (name = "page", defaultValue = "1") Integer page,
+                         @RequestParam (name = "size", defaultValue = "5") Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -44,8 +47,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/life/majiang/community/mapper/QuestionMapper.java
+++ src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -3,6 +3,7 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -12,6 +13,9 @@ public interface QuestionMapper {
     @Insert("INSERT INTO question (title, description, gmt_create, gmt_modified, creator, tag) VALUES (#{title}, #{description}, #{gmtCreate}, #{gmtModified}, #{creator}, #{tag})")
     void create(Question question);
 
-    @Select("SELECT * FROM question")
-    List<Question> list();
+    @Select("SELECT * FROM question LIMIT #{offset}, #{size}")
+    List<Question> list(@Param("offset") Integer offset, @Param("size") Integer size);
+
+    @Select("SELECT COUNT(1) FROM question")
+    Integer count();
 }
